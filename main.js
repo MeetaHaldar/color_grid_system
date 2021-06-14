a0de5b2e8420616881e3c829bcc3fca6c87c8b4d
meeta haldar <meetahaldar1001@gmail.com>
2021-06-14 19:24:14 +0530

    added all function of js

--- main.js
+++ main.js
@@ -55,305 +55,306 @@
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction8() {
+        var copyText = document.getElementById("myInput8");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction9() {
+        var copyText = document.getElementById("myInput9");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction10() {
+        var copyText = document.getElementById("myInput10");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction11() {
+        var copyText = document.getElementById("myInput11");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction12() {
+        var copyText = document.getElementById("myInput12");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
 
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+    
+      function myFunction13() {
+        var copyText = document.getElementById("myInput13");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction14() {
+        var copyText = document.getElementById("myInput14");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction15() {
+        var copyText = document.getElementById("myInput15");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction16 () {
+        var copyText = document.getElementById("myInput16");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction17() {
+        var copyText = document.getElementById("myInput17");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction18() {
+        var copyText = document.getElementById("myInput18");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction19() {
+        var copyText = document.getElementById("myInput19");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction20() {
+        var copyText = document.getElementById("myInput20");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction21() {
+        var copyText = document.getElementById("myInput21");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction22() {
+        var copyText = document.getElementById("myInput22");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction23() {
+        var copyText = document.getElementById("myInput23");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
 
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction24() {
+        var copyText = document.getElementById("myInput24");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction25() {
+        var copyText = document.getElementById("myInput25");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction26() {
+        var copyText = document.getElementById("myInput26");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction27() {
+        var copyText = document.getElementById("myInput27");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction28() {
+        var copyText = document.getElementById("myInput28");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Copied the text: " + copyText.value);
       }
-      function myFunction1() {
-        var copyText = document.getElementById("myInput1");
+      function myFunction29() {
+        var copyText = document.getElementById("myInput29");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction30() {
+        var copyText = document.getElementById("myInput30");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction31() {
+        var copyText = document.getElementById("myInput31");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction32() {
+        var copyText = document.getElementById("myInput32");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction33() {
+        var copyText = document.getElementById("myInput33");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction34() {
+        var copyText = document.getElementById("myInput34");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction35() {
+        var copyText = document.getElementById("myInput35");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction36() {
+        var copyText = document.getElementById("myInput36");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction37() {
+        var copyText = document.getElementById("myInput37");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction38() {
+        var copyText = document.getElementById("myInput38");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction39() {
+        var copyText = document.getElementById("myInput39");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction40() {
+        var copyText = document.getElementById("myInput40");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction41() {
+        var copyText = document.getElementById("myInput41");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction42() {
+        var copyText = document.getElementById("myInput42");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction43() {
+        var copyText = document.getElementById("myInput43");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction44() {
+        var copyText = document.getElementById("myInput44");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+
+      function myFunction45() {
+        var copyText = document.getElementById("myInput45");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction46() {
+        var copyText = document.getElementById("myInput46");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction47() {
+        var copyText = document.getElementById("myInput47");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction48() {
+        var copyText = document.getElementById("myInput48");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction49() {
+        var copyText = document.getElementById("myInput49");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction50() {
+        var copyText = document.getElementById("myInput50");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
